30cec03a83174b15f310dfda89f01132a77be9b5
Tim <tim.langner@tobit.software>
2019-09-13 17:20:45 +0200

    :sparkles: Finish database

---
+++ Company/CompanyDb/Stored Procedure/Company/spCreateCompany.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spCreateCompany]
+	@CompanyName nvarchar(256),
+	@FoundedDate date
+AS
+
+	INSERT INTO [Company]	
+				(
+					[Name],
+					[FoundedDate]
+				) 
+	VALUES		
+				(
+					@CompanyName,
+					@FoundedDate
+				)
+
+SELECT SCOPE_IDENTITY();
