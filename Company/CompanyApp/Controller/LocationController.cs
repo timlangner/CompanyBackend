d9fb8d6af63a53719ab499f3cef652e77615ccea
Tim <tim.langner@tobit.software>
2019-09-13 17:32:28 +0200

    :sparkles: Add read company

---
+++ Company/CompanyApp/Controller/LocationController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using CompanyApp.Interface;
+using CompanyApp.Model;
+using CompanyApp.Repository;
+
+namespace CompanyApp.Controller
+{
+    public class LocationController
+    {
+        readonly IBaseInterface<Company> _companyRepository;
+
+        public LocationController(string DbConStr)
+        {
+            _companyRepository = new CompanyRepository(DbConStr);
+        }
+
+        public Company Create(Company company)
+        {
+            return _companyRepository.Create(company);
+        }
+
+        public List<Company> Read()
+        {
+            return _companyRepository.Read();
+        }
+
+        public Company Read(int id)
+        {
+            return _companyRepository.Read(id);
+        } 
+
+        public Company Update(Company company)
+        {
+            return _companyRepository.Update(company);
+        }
+
+        public bool Delete(int id)
+        {
+            return _companyRepository.Delete(id);
+        }
+
+    }
+}
