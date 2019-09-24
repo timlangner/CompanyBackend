e9280a86bd12f22d1d427d2675367015844c8bf7
Tim <tim.langner@tobit.software>
2019-09-24 14:20:29 +0200

    :zap: Add middleware class + middleware extension class

---
+++ CompanyDb/CompanyAPI/Middleware/RepoExceptionMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyAPI.Middleware
+{
+    public class RepoExceptionMiddlewareExtensions
+    {
+        public void UseRepoExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RepoExceptionMiddleware>();
+        }
+    }
+}
